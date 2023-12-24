75a3458dfbfc6a082d6192010d669e6108c7af5a
MDNirobkhan1 <nirob937612@gmail.com>
2023-12-24 15:34:15 +0600

    cart location

--- script.js
+++ script.js
@@ -14,20 +14,29 @@ if(close){
     })
 }
 
-// single Product 
+// single Product
 
-const mainImg=document.getElementById("MainImg");
-const smallImg= document.getElementsByClassName("small-img")
+// const mainImg=document.getElementById("MainImg");
+// const smallImg= document.getElementsByClassName("small-img")
 
-smallImg[0].onclick= function(){
-    mainImg.src=smallImg[0].src;
-}
-smallImg[1].onclick= function(){
-    mainImg.src=smallImg[1].src;
-}
-smallImg[2].onclick= function(){
-    mainImg.src=smallImg[2].src;
-}
-smallImg[3].onclick= function(){
-    mainImg.src=smallImg[3].src;
-}
+// smallImg[0].onclick= function(){
+//     mainImg.src=smallImg[0].src;
+// }
+// smallImg[1].onclick= function(){
+//     mainImg.src=smallImg[1].src;
+// }
+// smallImg[2].onclick= function(){
+//     mainImg.src=smallImg[2].src;
+// }
+// smallImg[3].onclick= function(){
+//     mainImg.src=smallImg[3].src;
+// }
+
+
+
+// add to cart data funstion
+
+
+// 
+
+
